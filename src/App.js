f1bf0b9ead83bf80560ec80ba1f1056ad3cebf86
skbhoi2001 <soumyak.bhoi.2001@gmail.com>
2021-11-13 08:29:58 +0530

    !

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import OrderList from './OrderList/OrderList';
+
+function App() {
+  return (
+    <div className="App App-header">
+        <OrderList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/OrderList/OrderList.jsx
@@ -0,0 +1,14 @@
+export default function OrderList(){
+    return(
+        <>
+        <h4>
+            <ol>
+                <li>item1</li>
+                <li>item2</li>
+                <li>item3</li>
+            </ol>
+        </h4>
+    </>
+    )
+    
+}
